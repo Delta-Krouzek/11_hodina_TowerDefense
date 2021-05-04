446b954ec10a151265ef0c7c249c18671d9aa086
czM1K3 <misamadera1@gmail.com>
2021-05-04 17:08:12 +0200

    Polovina hodiny

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -10,31 +10,52 @@ public class Turret : MonoBehaviour
 
     public string enemyTag = "Enemy";
 
+    public Transform rotateAround;
+    public float rotationSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        //Vector3 rotation = lookRotation.eulerAngles;
+        Vector3 rotation = Quaternion.Lerp(rotateAround.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
+        rotateAround.rotation = Quaternion.Euler(0, rotation.y, 0);
     }
 
     void UpdateTarget()
     {
         var enemies = GameObject.FindGameObjectsWithTag(enemyTag);
         float shortestDistance = Mathf.Infinity;
-        GameObject neresetEnemy = null;
+        GameObject nearesetEnemy = null;
         foreach(var enemy in enemies)
         {
             float distance = Vector3.Distance(transform.position, enemy.transform.position);
             if (distance < shortestDistance)
             {
                 shortestDistance = distance;
-                neresetEnemy = enemy;
+                nearesetEnemy = enemy;
             }
+        }
+
+        if (nearesetEnemy != null && shortestDistance <= range)
+        {
+            target = nearesetEnemy.transform;
+        }
+        else
+        {
+            target = null;
         }
     }
 
