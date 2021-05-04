2cd72f2b119fa6b3137bba0122bd74ac9d731192
czM1K3 <misamadera1@gmail.com>
2021-05-04 17:37:35 +0200

    Dalsi hodina

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform target;
+    public float speed = 70f;
+
+    public void Init(Transform target)
     {
-        
+        this.target = target;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float dis = speed * Time.deltaTime;
+
+        if (dir.magnitude <= dis)
+        {
+            Destroy(target.gameObject);
+            Destroy(gameObject);
+            return;
+        }
+        transform.Translate(dir.normalized * dis, Space.World);
     }
 }
